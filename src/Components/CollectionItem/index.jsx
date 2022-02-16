387d422cc2ad4f3fa8d2521797611ef0d36b46e0
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-16 18:32:43 +0500

    Shop Page Styling

--- src/Components/CollectionItem/index.jsx
+++ src/Components/CollectionItem/index.jsx
@@ -4,7 +4,7 @@ import "./style.scss";
 const CollectionItem = ({ name, price, imageUrl }) => {
     return (
         <div className="collection-item">
-            <div className="image" style={{ background: `url("${imageUrl}")` }}></div>
+            <div className="image" style={{ backgroundImage: `url("${imageUrl}")` }}></div>
             <div className="collection-footer">
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
