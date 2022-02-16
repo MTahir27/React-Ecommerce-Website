e55ec3c761d55537d09ddd7e5f892cad0bb86d7c
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-16 10:48:19 +0500

    Navbar Linked

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Pages/Home";
 import ShopPage from "./Pages/Shop";
 import SigninSignup from "./Pages/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/shop" element={<ShopPage />} />
-          <Route path="/login" element={<SigninSignup />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/login" element={<SigninSignup />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import "./style.scss";
 import { Link } from "react-router-dom";
+import "./style.scss";
 import { ReactComponent as Logo } from "../../assets/images/logo/logo.svg";
 
 const Navbar = () => {
     return (
-        <nav>
-            <Logo className="logo" />
-            <h1>Navbar</h1>
-        </nav>
+        <header className="header">
+            <Link className="logo-container" to="/">
+                <Logo className="logo" />
+            </Link>
+            <section className="options">
+                <Link className="option" to="/shop" >Shop</Link>
+                <Link className="option" to="/contact" >Contact</Link>
+            </section>
+        </header>
     )
 }
 
