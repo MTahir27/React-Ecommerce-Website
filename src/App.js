21581f8235b8857f64078f26665afc6df5a76e02
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-16 10:35:46 +0500

    Routes Added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Navbar from "./Components/Navbar";
 import HomePage from "./Pages/Home";
 import ShopPage from "./Pages/Shop";
 import SigninSignup from "./Pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
-      <ShopPage />
-      <SigninSignup />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/login" element={<SigninSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
