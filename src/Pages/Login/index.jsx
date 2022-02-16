80f87e266e2ff78dd90be3350303b7f94785a98d
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-16 10:25:04 +0500

    Pages Created

---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.scss";
+
+const SigninSignup = () => {
+    return (
+        <h1>Login Signin Page</h1>
+    )
+}
+
+export default SigninSignup;
---
+++ src/Pages/Shop/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.scss";
+
+class ShopPage extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <h1>Shop Page</h1>
+        )
+    }
+}
+
+export default ShopPage;
