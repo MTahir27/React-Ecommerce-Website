212ce67c3f525607184ae58f2f882b253006e9fc
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-16 17:25:13 +0500

    Collection Preview Component Created

---
+++ src/Components/CollectionPreview/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.scss";
+import CollectionItem from "../CollectionItem";
+
+const CollectionPreview = () => {
+    return (
+        <CollectionItem />
+    )
+}
+
+export default CollectionPreview;
