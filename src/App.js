2591765c2d39a5adffa1856aed00bb874249c17c
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-16 10:29:18 +0500

    Page Linked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import HomePage from "./Pages/Home";
+import ShopPage from "./Pages/Shop";
+import SigninSignup from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HomePage />
+      <ShopPage />
+      <SigninSignup />
     </div>
   );
 }
