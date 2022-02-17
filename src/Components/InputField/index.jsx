3c97bdb8d2b60e990da9b596f66084644728a90d
MTahir27 <m.tahiridrees27@gmail.com>
2022-02-17 18:35:17 +0500

    Input Field Styling

--- src/Components/InputField/index.jsx
+++ src/Components/InputField/index.jsx
@@ -5,8 +5,7 @@ const InputField = ({ handleChange, label, ...otherInputProps }) => {
     return (
         <div className="group">
             {
-                label ? (<label className={`${props.value.length ? "shrink" : null} form-input-label`} htmlFor={label}>{label}</label>) : null
-
+                label ? (<label className={`${otherInputProps.value.length ? "shrink" : null} form-input-label`} htmlFor={label}>{label}</label>) : null
             }
             <input className="form-input" onChange={handleChange} {...otherInputProps} />
         </div>
